91798a7208703c0a90560412409aab8ddc936894
Nagi <62817358+ranjan-sri@users.noreply.github.com>
2025-01-03 14:27:23 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Configuration file for API keys
 
-const serpApiKey = '20f1b46ece7fe906cf5289b12bb1fc1695b736ccaa80cd05a9d0ce58ceb4fd9f'; // Replace with your actual SerpApi key
-const imgurClientId = '4cd3308b6a68dd8'; // Replace with your actual Imgur Client ID
-console.log('config.js loaded');
+const serpApiKey = 'xxxxx'; // Replace with your actual SerpApi key
+const imgurClientId = 'xxxxxx'; // Replace with your actual Imgur Client ID
